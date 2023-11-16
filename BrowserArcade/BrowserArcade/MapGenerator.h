49ba3282c6b11831f4cbb5bc57d3d35a9d74b549
MIXED-FresH <freshmanmax4@gmail.com>
2023-11-16 22:10:41 +0300

    Добавлены приватные члены в MapGenerator Объявлены некоторые публичные функции и конструктор

--- BrowserArcade/BrowserArcade/MapGenerator.h
+++ BrowserArcade/BrowserArcade/MapGenerator.h
@@ -1,5 +1,24 @@
 #pragma once
 
-class MapGenerator {
+struct mapSize
+{
+	unsigned short X;
+	unsigned short Y;
+};
 
+class MapGenerator {
+private:
+	mapSize mapSize;
+	unsigned short roomsAmount;
+	unsigned short minRoomSquare;
+	int randSeed;
+
+public:
+	MapGenerator(unsigned short& sizeX, unsigned short& sizeY,
+		unsigned short& roomsAmount, unsigned short& minSquare, int randSeed = 1) :
+		mapSize{ sizeX, sizeY }, roomsAmount(roomsAmount),
+		minRoomSquare(minSquare), randSeed(randSeed) {};
+	void setNewMap(unsigned short& sizeX, unsigned short& sizeY,
+		unsigned short& rooms, unsigned short& minSquare);
+	void setRandomSeed(int& seed);
 };
